545cf7f3556c5d1e1157f77e1cfad49f148ab4be
D4LM <148903901+D4LMwastaken@users.noreply.github.com>
2024-08-25 12:45:36 -0400

    Changed from 2.6 to 2.7

--- DiscordBot2.7.py
+++ DiscordBot2.7.py
@@ -1,81 +1,84 @@
+# DiscordBot2.7.py
+"""
+    Version 2.7 of the Python DiscordBot programmed by D4LM.
+    Please read the README.md for installation, contact information, changelog, and installation.
+"""
 # Dependencies
-import discord  # Instructions to install/website: https://discordpy.readthedocs.io/en/stable/
-import os  # Built in module in Python that is needed to get the .env file.
-import random  # Built-in Python module that allows some items, like greetings and goodbyes, to be random.
-import Gemini  # A function library attached to the project file that is labeled "Gemini.py".
-# This function library is necessary to make the !Ask Gemini command work.
+import discord  # Website: https://discordpy.readthedocs.io/en/stable/
+import os  # Built in Python module. Used to get the .env file.
+import random  # Built in Python module. Allows me to use randomness in my Python Script.
+import Gemini  # A function library attached to the DiscordBot2.7.py folder. Needed to use the !Ask Gemini command.
+
+# .env file support
 from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env.
 
-# DiscordBot2.6.py
-"""
-    Version 2.6 of the Python Discord Bot coded by D4LM
-    Please read the ReadMe.MD for installation, issues, changelog and other information about this Python file.
-"""
-Version = "2.6 (Developer Edition)"
-"""
-    Note: Change version every time you make modifications to the code. Change the 
-    edition if you are using for testing/developing purposes (Developer Edition) or are running a stable file for the 
-    server-side (Release Edition) 
-"""
-DateUpdated = "7/21/2024"  # Change the release date to the last time you made modifications to the code
-Language = "Python"  # Change the language only if your transcode it into another language.
-Dev = "D4LM"  # Change the developer only if you modified the code at least 50%.
-CoDev = ""  # For maintainers or people who helped code at least 5% of this Python file.
-UpdatedItems = ("Second version. " + "\n" + "This version added a built-io 2000 character limit for Gemini AI. Will "
-                                            "not exceed 2000 character limit unless you tell it to do so.")
+load_dotenv()  # Get the environment variables from the .env you added.
 
+# About the project variables
+Version = "2.7"  # Note: Change version every time you make modifications to code. Merged developer and
+# release editions into one.
+DateUpdated = "8/25/2024"  # Change the release date into the last time you updated.
+Language = "Python"  # Change the language only if the code is in a different version or variant.
+Dev = "D4LM"  # Change if you are the one who creates a fork of this project.
+CoDev = ""  # This is a place to put maintainers of your project.
+UpdatedItems = ("Third version. " + "\n" + "Organized DiscordBot 2.7.py to be more clear. "
+                                           "Added !Ask GeminiPro command.")
+
+# Discord package required code
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-# Lists for the Discord Bot (General)
-greetings = ["Hello!", "What's up!", "Howdy!", "Greetings!"]  # List of greetings to use.
-goodbyes = ["Bye!", "Goodbye!", "See you later!", "See you soon!"]  # List of goodbyes to use.
+# Lists the Discord Bot uses
+greetings = ["Hello!", "What's up!", "Howdy!", "Greetings!"]
+goodbyes = ["Bye!", "Goodbye!", "See you later!", "See you soon!"]
 commands = ["!Music", "!Pet", "!Book", "!Video Games", "!PING", "!Job", "!About Me", "!Bye", "!Help", "!Version",
-            "!New Stuff", "!Self Destruct", "!Ask Gemini"]  # Put new commands here
-
-# Keywords and responses lists
+            "!New Stuff", "!Self Destruct", "!Ask Gemini"]  # Put new commands here.
 keywords = ["!Music", "!Pet", "!Book", "!Video Games", "!Ping", "!Job", "!About Me"]
 responses = ["Music is so relaxing!", "Dogs are man's best friend!", "I know a lot of books.",
              "I like to play video games. My favorite are World of Warships, Minecraft, 0AD and Roblox.", "Pong!",
              "Help and advise the Largo High School Coding Club Discord server.",
              "A Discord bot made by D4LM who is smart and amazing."]
 """
-    To add more items ot the lists add a comma after the item, making sure the item is enclosed in quotation marks.
-    The two arrays work by saying for instance, "!Music", it will reply typing "Music is so relaxing".
-    Recommended for coders to use to make it easier to add commands are very short and simple like !PING.
+    To add more items to the lists add a comma after hte item, making sure the item is enclosed in quotation marks.
+    The two lists, keywords and responses work by when a Discord user sends a message with one of the items in the
+        keywords list, the keywords list being indexed from 0 to the last item (!Music=0, !Pet =1, etc.). Then it will 
+        take the index location of that keyword and put it into the response list with the index being the index 
+        location of the keyword. 
+    Recommended if you want to make a basic command without having to make an if statement for each one.
 """
 
 
-# Functions
+# Required functions
 def random_list_item(chosen_list):
     """
-        Necessary function required to make sure the chosen elements in the list are random.
-        If this function is used inside client event it will only work once and stop being random.
+    Necessary function required.
+    Used to make sure the chosen elements in the list are random.
+    If used inside @Client.event, it will stop certain replies from being random.
     """
-    chosen_integer = random.randrange(0, len(chosen_list)) 
-    chosen_element = chosen_list[chosen_integer]
-    return chosen_element
+    chosen_number = random.randrange(0, len(chosen_list))
+    chosen_item = chosen_list[chosen_number]
+    return chosen_item
 
 
+# Required. Starts DiscordBot code.
 @client.event
 async def on_ready():
-    # Test statement to tell the developer and make sure that the bot went into the Discord Server and went under
-    # what username/display name.
     print(f'We have logged in as {client.user}')
+    # Function that contacts Discord and gives what username/display name the bot is under.
 
 
+# Rest of the coded for the Discord Bot
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        # Stops the Discord Bot from replying to itself.
         return
+        # Required if statement. Stops the Discord Bot from replying to itself.
 
     if '!HI' in str.upper(message.content):
-        # If user sends '!HI' or similar, the bot will say a random greeting.
         await message.channel.send(random_list_item(greetings))
+        # If a user sends '!HI' or similarly, the bot will say a random greeting.
 
     if '!HELP' in str.upper(message.content):
         # Send out all the possible commands for the Discord Bot.
@@ -119,8 +122,16 @@ async def on_message(message):
         await message.channel.send('What do you want to ask? Please do not ask for something more than 2000 '
                                    'characters.')
         reply = await client.wait_for("message")
-        answer = Gemini.genai_question(reply.content)
-        await message.channel.send(answer)
+        if "!PRO" in str.upper(reply.content):
+            await message.channel.send(
+                'What do you want to ask for the pro version? Please do not ask a question that will'
+                ' generate more than 2000 characters.')
+            reply = await client.wait_for("message")
+            answer = Gemini.genai_question_pro(reply.content)
+            await message.channel.send(answer)
+        else:
+            answer = Gemini.genai_question(reply.content)
+            await message.channel.send(answer)
 
     for index, keyword in enumerate(keywords):
         """
--- Gemini.py
+++ Gemini.py
@@ -1,9 +1,9 @@
-import os  # Built in module in Python that is needed to get the .env file. Not used in my ide but necessary for other
-# developers who develop on a different ide/code editor.
+import os  # Built in module in Python that is necessary to get the .env file.
 
 import google.generativeai as genai  # Import the necessary module that the
 
 from dotenv import load_dotenv
+
 load_dotenv()  # take environment variables from .env.
 
 # !Ask Gemini command and this function library to work. Website: https://ai.google.dev/gemini-api
@@ -11,22 +11,38 @@ load_dotenv()  # take environment variables from .env.
 
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')  # Environmental variable that you should have got from Google AI Studio.
 
-genai.configure(api_key=GOOGLE_API_KEY)  # Tells the module which one of your variables is the Google API Keu
-
-model = genai.GenerativeModel('gemini-1.5-flash')  # Model that the !Ask Gemini uses.
-model.max_output_tokens = 250  # Forces the AI to use no more than 250 tokens so that the AI model does not overthink
-# on a single question and cannot answer any questions later.
+genai.configure(api_key=GOOGLE_API_KEY)  # Tells the module which one of your variables is the Google AI Keu
 
 
 def genai_question(question):  # Function used in the main script
+    model = genai.GenerativeModel('gemini-1.5-flash')  # Model that the !Ask Gemini uses.
     response = model.generate_content("In less than 2000 characters." + question)  # The "In less than 2000 characters"
     # just tells the bot to not type more than 2000 characters.
-    print(response.text)
-    if response.text == "":  # Tells why the message could not be sent
+    print(response)
+    if response == "":  # Tells why the message could not be sent
+        print("Unable to receive message, check message to make sure it is safe or not...")
         return "Unable to receive message, check message to make sure it is safe or not..."
     elif len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
         # by Discord
         print("text too long")
         return "The text is too long to be sent, try another answer that takes up less text."
     else:
-        return response.text  # Sends the response from the AI bot back to the command that needs it.
+        print(response.text)
+        return response.text
+
+
+def genai_question_pro(question):
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    response = model.generate_content("In less than 2000 characters." + question)
+    # just tells the bot to not type more than 2000 characters.
+    print(response)
+    if response == "":  # Tells why the message could not be sent
+        print("Unable to receive message, check message to make sure it is safe or not...")
+        return "Unable to receive message, check message to make sure it is safe or not..."
+    elif len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
+        # by Discord
+        print("text too long")
+        return "The text is too long to be sent, try another answer that takes up less text."
+    else:
+        print(response.text)
+        return response.text
