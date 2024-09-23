a1984268bdffe5c458017786bd5bb9c652767891
D4LM <148903901+D4LMwastaken@users.noreply.github.com>
2024-09-22 21:01:37 -0400

    Fixed bug

--- cogs/Gemini.py
+++ cogs/Gemini.py
@@ -16,34 +16,26 @@ def genai_question(question):  # Function used in the main script
     model = genai.GenerativeModel('gemini-1.5-flash')  # Model that the !Ask Gemini uses.
     response = model.generate_content("In less than 2000 characters." + question)  # The "In less than 2000 characters"
     # just tells the bot to not type more than 2000 characters.
-    print(response)
-    if response == "":  # Tells why the message could not be sent
-        print("Unable to receive message, check message to make sure it is safe or not...")
-        return "Unable to receive message, check message to make sure it is safe or not..."
-    elif len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
+    if len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
         # by Discord
         print("text too long")
         return "The text is too long to be sent, try another answer that takes up less text."
     else:
-        print(response.text)
-        return response.text
+        print(response)
+        return response
 
 
 def genai_question_pro(question):
     model = genai.GenerativeModel('gemini-1.5-pro')
     response = model.generate_content("In less than 2000 characters." + question)
     # just tells the bot to not type more than 2000 characters.
-    print(response)
-    if response == "":  # Tells why the message could not be sent
-        print("Unable to receive message, check message to make sure it is safe or not...")
-        return "Unable to receive message, check message to make sure it is safe or not..."
-    elif len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
+    if len(response.text) >= 2000:  # Creates an else if statement to check also if the text is too long to be sent
         # by Discord
         print("text too long")
         return "The text is too long to be sent, try another answer that takes up less text."
     else:
-        print(response.text)
-        return response.text
+        print(response)
+        return response
 
 
 class Gemini(commands.Cog):
@@ -54,11 +46,15 @@ class Gemini(commands.Cog):
 
     @gemini.command(name="ask", description="Ask Gemini a question")
     async def ask_gemini(self, ctx, question: str):
-        await ctx.respond(genai_question(question))
+        await ctx.respond("You asked: "+question)
+        response = genai_question(question)
+        await ctx.send(response.text)
 
     @gemini.command(name="ask_pro", description="Ask Gemini Pro Model a question")
     async def ask_gemini_pro(self, ctx, question: str):
-        await ctx.respond(genai_question_pro(question))
+        await ctx.respond("You asked: "+question)
+        response = genai_question_pro(question)
+        await ctx.send(response.text)
 
 
 def setup(bot):
